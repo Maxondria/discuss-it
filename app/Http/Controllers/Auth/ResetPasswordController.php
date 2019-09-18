155954713917afc4975e70dbfc354e530a2a308f
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-18 17:58:30 +0300

    [task] configure .env to update app name and connect to DB

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace DiscussIt\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use DiscussIt\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
--- config/app.php
+++ config/app.php
@@ -169,11 +169,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        DiscussIt\Providers\AppServiceProvider::class,
+        DiscussIt\Providers\AuthServiceProvider::class,
+        // DiscussIt\Providers\BroadcastServiceProvider::class,
+        DiscussIt\Providers\EventServiceProvider::class,
+        DiscussIt\Providers\RouteServiceProvider::class,
 
     ],
 
